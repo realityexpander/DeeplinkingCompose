37cc04e3880db49c1f9bc78db5157c9c3dd3ebcd
Chris Athanas <realityexpander@gmail.com>
2023-02-14 02:55:01 -0600

    Improve manifest

--- app/src/main/java/com/realityexpander/deeplinkingcompose/MainActivity.kt
+++ app/src/main/java/com/realityexpander/deeplinkingcompose/MainActivity.kt
@@ -17,12 +17,18 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
 import com.realityexpander.deeplinkingcompose.ui.theme.DeeplinkingComposeTheme
+import android.net.Uri
 
 // Deep link validation are kept on github website:
 // https://github.com/realityexpander/realityexpander.github.io
 // in /.well_known/assetlinks.json
 
 // Source video: https://www.youtube.com/watch?v=z6VlP0o_sDc&list=WL&index=37&t=1s
+
+// To test from shell
+//adb shell am start -a android.intent.action.VIEW \                                       1381  02:46:07 
+// -c android.intent.category.BROWSABLE \
+// -d "https://realityexpander.github.io/44"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
